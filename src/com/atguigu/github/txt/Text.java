68f3f0bc2945a905d168e9a3d5278aa09e97a660
xmy0520 <1124118351@qq.com>
2016-09-18 20:23:29 +0800

    1111

--- src/com/atguigu/github/txt/Text.java
+++ src/com/atguigu/github/txt/Text.java
@@ -3,6 +3,6 @@ package com.atguigu.github.txt;
 public class Text {
 	public static void main(String[] args) {
 		System.out.println("hello github");
-		System.out.println("hello 0427");
+		System.out.println("hello 0427 zz");
 	}
 }
